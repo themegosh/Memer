c1929f98d4a486dc2fcced5f7f0b60012a45fab1
themegosh <mathewdf@gmail.com>
2015-12-18 01:45:34 -0500

    Add errors help streamline image uploading

--- util/js/create.js
+++ util/js/create.js
@@ -7,26 +7,37 @@ function uploadEx() {
     var disableInputs = document.getElementsByClassName('saveDisable');
     var saveSuccess = document.getElementById('saveSuccess');
     var title = document.getElementById('title');
+    var topText = document.getElementById('topText');
+    var bottomText = document.getElementById('bottomText');
+    
+    if (title.value.trim() == ""){
+        notification.innerHTML = "<div class='alert alert-danger'>Error: You must provide a title...</div>";
+        return;
+    }
+    if (topText.value.trim() == "" && bottomText.value.trim() == ""){
+        notification.innerHTML = "<div class='alert alert-danger'>Error: You havent given the image any text...</div>";
+        return;
+    }
 
+
+    disableInputs.disabled = true;
+    
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        notification.innerHTML = xhr.responseText;
-        //window.location.replace("memes.php?txtSearch="+title.value);
+        if (xhr.readyState == 4) {
+            notification.innerHTML = xhr.responseText;
+            disableInputs.disabled = false;
+        }
     }
-}
     xhr.open('POST', 'ajaxCreate.php', true);
 
     xhr.upload.onprogress = function(e) {
         if (e.lengthComputable) {
-            var percentComplete = (e.loaded / e.total) * 100;
-            console.log(percentComplete + '% uploaded');
-            console.log('Succesfully uploaded');
+            var percentComplete = Math.round((e.loaded / e.total) * 100);
+            notification.innerHTML = "<div class='alert alert-warning'>Uploading... "+percentComplete+ " % done.</div>";
         }
     };
     
-    
-
     xhr.onload = function() {
 
     };
